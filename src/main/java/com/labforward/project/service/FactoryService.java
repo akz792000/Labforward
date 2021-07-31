d72c13498358e0b15abe1c4395ce33e5c94d4898
a.zandi <a.zandi@ocd.co.ir>
2021-07-31 19:29:12 +0430

    Generic Service

--- src/main/java/com/labforward/project/service/FactoryService.java
+++ src/main/java/com/labforward/project/service/FactoryService.java
@@ -2,35 +2,18 @@ package com.labforward.project.service;
 
 import com.labforward.project.domain.FactoryEntity;
 import com.labforward.project.repository.FactoryRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 /**
  * @author Ali Karimizandi
  * @since 2021
  */
-@RequiredArgsConstructor
+
 @Service
-public class FactoryService {
+public class FactoryService extends GenericService<FactoryRepository, FactoryEntity, Long> {
 
-    private final FactoryRepository repository;
-
-    public Optional<FactoryEntity> findById(Long id) {
-        return repository.findById(id);
-    }
-
-    public Optional<FactoryEntity> findByCode(Long id) {
-        return repository.findByCode(id);
-    }
-
-    public FactoryEntity save(FactoryEntity entity) {
-        return repository.save(entity);
-    }
-
-    public void deleteById(Long id) {
-        repository.deleteById(id);
+    public FactoryService(FactoryRepository repository) {
+        super(repository);
     }
 
 }
